f3a99aa06d1942d1756aadc5d5c46541cfcd1c8a
ukasz1 <lukaszmitkowski53@gmail.com>
2023-11-10 14:26:34 +0100

    Add candlestick chart with mocked data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import ApexChart from './components/charts/ApexChart';
 
 function App() {
   return (
-    <Navbar />
+    <div className="app">
+      <Navbar />
+      <div className="content">
+        <ApexChart />
+      </div>
+    </div>
   );
 }
 
