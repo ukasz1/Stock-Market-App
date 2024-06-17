4dd15fec1ae9fd7785214afc5e40fd68bfdc1b27
ukasz1 <lukaszmitkowski53@gmail.com>
2024-06-17 14:07:51 +0200

    Change App.jsx and index.jsx into TSX files

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root')
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
